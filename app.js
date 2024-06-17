54d570392c914993cb0c9ca5f3e0ee0ea3091376
samriddhi <samriddhisahu1206@gmail.com>
2024-06-18 01:31:49 +0530

    my second  commit

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.use('/' , require('./server/routes/main'));
 
 
 app.listen(PORT, ()=> {
-    console.log(`App listening on port ${PORT}`);
+    console.log(`App listening on the port ${PORT}`);
 })
