b6fb58f295f78f31e28f5edd575b60e77eb49e49
samriddhi <samriddhisahu1206@gmail.com>
2024-06-19 00:12:52 +0530

    my test1  inital  commit

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
+
+
+
+
+
+
+
+
+
 
 //Routes
-router.get('',(req,res) => {
-    const locals = {
+router.get('', async (req,res) => {
+   
+    try {
+         const locals = {
         title: "NodeJs Blog",
         description: "Simple Blog created with NodeJs ,Express and MongoDb."
     }
-    res.render('index' ,{locals});
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([ {$sort: {createdAt: -1}}])
+.skip(perPage * page - perPage)
+.limit(perPage)
+.exec();
+
+ const count = await Post.count();
+ const nextPage = parseInt(page) + 1;
+
+ const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+   
+            
+             res.render('index', { 
+                locals,
+                 data,
+                current: page,
+            nextPage: hasNextPage ? nextPage : null
+         });
+          } catch (error) {
+            console.log(error);
+           }
 });
+
+
+/*function insertPostData () {
+    Post.insertMany([
+{
+    title:"Building a Blog",
+    body: "This is the body text"
+},
+
+])
+}
+
+insertPostData();*/
+
+
+
 
 router.get('/about',(req,res) => {
     res.render('about');
